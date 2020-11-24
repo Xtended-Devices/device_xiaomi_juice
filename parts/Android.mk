dc98633d516486204af688e525f74d25411b909d
Jeong Seong Pil <jjjhitel@gmail.com>
2020-11-25 02:41:39 +0900

    picasso: Add touchfeature implementation of DT2W

--- parts/Android.mk
+++ parts/Android.mk
@@ -18,6 +18,10 @@ LOCAL_STATIC_ANDROID_LIBRARIES := \
     androidx.core_core \
     androidx.preference_preference
 
+LOCAL_STATIC_JAVA_LIBRARIES := \
+    vendor.xiaomi.hardware.displayfeature-V1.0-java \
+    vendor.xiaomi.hardware.touchfeature-V1.0-java
+
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 LOCAL_RESOURCE_DIR := \
--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -19,15 +19,31 @@ package org.lineageos.settings;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.util.Log;
 
 import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
 import org.lineageos.settings.utils.RefreshRateUtils;
 
+import vendor.xiaomi.hardware.touchfeature.V1_0.ITouchFeature;
+
 public class BootCompletedReceiver extends BroadcastReceiver {
+
+    public static final String SHAREDD2TW = "sharadeD2TW";
+    private ITouchFeature mTouchFeature;
 
     @Override
     public void onReceive(final Context context, Intent intent) {
+    //Micro-Service to restore sata of dt2w on reboot
+    SharedPreferences prefs = context.getSharedPreferences(SHAREDD2TW, Context.MODE_PRIVATE);
+    try {
+        mTouchFeature = ITouchFeature.getService();
+        mTouchFeature.setTouchMode(14,prefs.getInt(SHAREDD2TW, 1));
+    } catch (Exception e) {
+        // Do nothing
+    }
+
         // Refresh rate
         RefreshRateUtils.setFPS(RefreshRateUtils.getRefreshRate(context));
 
