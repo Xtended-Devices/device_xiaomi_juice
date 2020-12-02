1e57fe2bc9a56677cf5d645de090b23f264744a9
masemoel <az.az.az.mjme@gmail.com>
2020-12-02 19:36:21 +0100

    picasso: Fix building error in parts
    Signed-off-by: masemoel <az.az.az.mjme@gmail.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -36,7 +36,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(final Context context, Intent intent) {
-        if (DEBUG) Log.d(TAG, "Received boot completed intent");
         DiracUtils.initialize(context);
     //Micro-Service to restore sata of dt2w on reboot
     SharedPreferences prefs = context.getSharedPreferences(SHAREDD2TW, Context.MODE_PRIVATE);
