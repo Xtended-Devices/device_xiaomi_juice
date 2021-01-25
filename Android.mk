5fb2028fd1fe5078824fc82b57f3451ff5f50dc1
Chenyang Zhong <zhongcy95@gmail.com>
2021-01-25 05:24:14 -0500

    raphael: create and symlink a dummy /metadata/apex
    Google moved apex sessions directory from /data/apex/sessions to
    /metadata/apex/sessions after commit:
    
    "Move apex sessions directory to /metadata"
    https://android.googlesource.com/platform/system/apex/+/36cf4bbac6108898a531aacc102dcc489d791b37
    
    Devices with a mounted metadata partition will have the needed
    directories set up by system/core/rootdir/init.rc. Xiaomi devices
    on sdm845 do not have a metadata partition out of the box, so things
    like "Google Play system update" will fail to install the update.
    
    Therefore, create a dummy directory under /data/vendor/metadata_apex
    and symlink it to /metadata/apex.
    
    The reason why the old /data/apex/sessions directory is not used
    for the symlink is that apexd will call migrateSessionsDirIfNeeded()
    to recursively copy things from the old directory to the new one.
    Creating the symlink from /data/apex/sessions may result in
    unintended behaviors.
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- Android.mk
+++ Android.mk
@@ -9,4 +9,13 @@ LOCAL_PATH := $(call my-dir)
 ifneq ($(filter picasso,$(TARGET_DEVICE)),)
   subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
   $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
+
+METADATA_SYMLINK := $(TARGET_ROOT_OUT)/metadata
+$(METADATA_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@echo "Creating $@"
+	@mkdir -p $(TARGET_ROOT_OUT)/metadata
+	$(hide) ln -sf /data/vendor/metadata_apex $@/apex
+
+ALL_DEFAULT_INSTALLED_MODULES += $(METADATA_SYMLINK)
+
 endif
--- init/init_picasso.cpp
+++ init/init_picasso.cpp
@@ -27,111 +27,144 @@
    IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#include <cstdlib>
 #include <fstream>
+#include <string.h>
+#include <sys/sysinfo.h>
 #include <unistd.h>
 #include <vector>
 
 #include <android-base/properties.h>
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
-#include <sys/stat.h>
-#include <sys/types.h>
 #include <sys/_system_properties.h>
 
-#include "property_service.h"
 #include "vendor_init.h"
 
 using android::base::GetProperty;
+using android::base::SetProperty;
 
-std::vector<std::string> ro_props_default_source_order = {
-    "",
-    "build.",
-    "bootimage.",
-    "odm.",
-    "product.",
-    "system.",
-    "system_ext.",
-    "vendor.",
+char const *heapstartsize;
+char const *heapgrowthlimit;
+char const *heapsize;
+char const *heapminfree;
+char const *heapmaxfree;
+char const *heaptargetutilization;
+
+constexpr const char *RO_PROP_SOURCES[] = {
+    nullptr,   "product.", "product_services.", "odm.",
+    "vendor.", "system.", "system_ext.", "bootimage.",
 };
 
-void property_override(char const prop[], char const value[], bool add = true) {
-    prop_info *pi;
-
-    pi = (prop_info *)__system_property_find(prop);
-    if (pi)
-        __system_property_update(pi, value, strlen(value));
-    else if (add)
-        __system_property_add(prop, strlen(prop), value, strlen(value));
-}
-
-	/* From Magisk@jni/magiskhide/hide_utils.c */
-static const char *snet_prop_key[] = {
-    "ro.boot.vbmeta.device_state",
-    "ro.boot.verifiedbootstate",
-    "ro.boot.flash.locked",
-    "ro.boot.selinux",
-    "ro.boot.veritymode",
-    "ro.boot.warranty_bit",
-    "ro.warranty_bit",
-    "ro.debuggable",
-    "ro.secure",
-    "ro.build.type",
-    "ro.build.tags",
-    "ro.build.selinux",
-    NULL
+constexpr const char *PRODUCTS[] = {
+    "picasso",
+    "picasso_48m",
 };
 
- static const char *snet_prop_value[] = {
-    "locked",
-    "green",
-    "1",
-    "enforcing",
-    "enforcing",
-    "0",
-    "0",
-    "0",
-    "1",
-    "user",
+constexpr const char *DEVICES[] = {
+    "Redmi K30 5G",
+    "Redmi K30i 5G",
+};
+
+constexpr const char *BUILD_DESCRIPTION[] = {
+    "coral-user 11 RQ1A.210105.003 7005429 release-keys",
+};
+
+constexpr const char *BUILD_FINGERPRINT[] = {
+    "google/coral/coral:11/RQ1A.210105.003/7005429:user/"
     "release-keys",
-    "1",
-    NULL
 };
 
- static void workaround_snet_properties() {
+constexpr const char *CLIENT_ID[] = {
+    "android-xiaomi",
+};
 
-     // Hide all sensitive props
-    for (int i = 0; snet_prop_key[i]; ++i) {
-        property_override(snet_prop_key[i], snet_prop_value[i]);
+void check_device()
+{
+    struct sysinfo sys;
+
+    sysinfo(&sys);
+
+    if (sys.totalram >= 5ull * 1024 * 1024 * 1024){
+        // from - phone-xhdpi-6144-dalvik-heap.mk
+        heapstartsize = "16m";
+        heapgrowthlimit = "256m";
+        heapsize = "512m";
+        heaptargetutilization = "0.5";
+        heapminfree = "8m";
+        heapmaxfree = "32m";
+    } else if (sys.totalram >= 7ull * 1024 * 1024 * 1024) {
+        // from - phone-xhdpi-8192-dalvik-heap.mk
+        heapstartsize = "24m";
+        heapgrowthlimit = "256m";
+        heapsize = "512m";
+        heaptargetutilization = "0.46";
+        heapminfree = "8m";
+        heapmaxfree = "48m";
     }
 }
 
-void property_override_multifp(char const buildfp[], char const systemfp[],
-	char const bootimagefp[], char const vendorfp[], char const odmfp[], char const value[])
-{
-	property_override(buildfp, value);
-	property_override(systemfp, value);
-	property_override(bootimagefp, value);
-	property_override(vendorfp, value);
-    property_override(odmfp, value);
+void property_override(char const prop[], char const value[], bool add = true) {
+  prop_info *pi;
+
+  pi = (prop_info *)__system_property_find(prop);
+  if (pi)
+    __system_property_update(pi, value, strlen(value));
+  else if (add)
+    __system_property_add(prop, strlen(prop), value, strlen(value));
+}
+
+void load_props(const char *model, bool is_48 = false) {
+  const auto ro_prop_override = [](const char *source, const char *prop,
+                                   const char *value, bool product) {
+    std::string prop_name = "ro.";
+
+    if (product)
+      prop_name += "product.";
+    if (source != nullptr)
+      prop_name += source;
+    if (!product)
+      prop_name += "build.";
+    prop_name += prop;
+
+    property_override(prop_name.c_str(), value);
+  };
+
+  for (const auto &source : RO_PROP_SOURCES) {
+    ro_prop_override(source, "device", is_48 ? PRODUCTS[1] : PRODUCTS[0], true);
+    ro_prop_override(source, "model", model, true);
+    ro_prop_override(source, "fingerprint", BUILD_FINGERPRINT[0], false);
+    ro_prop_override(nullptr, "fingerprint", BUILD_FINGERPRINT[0], false);
+    ro_prop_override(nullptr, "description", BUILD_DESCRIPTION[0], false);
+    if (!is_48) {
+      ro_prop_override(source, "name", PRODUCTS[0], true);
+    } else {
+      ro_prop_override(source, "name", PRODUCTS[1], true);
+    }
+  }
+  ro_prop_override(nullptr, "description", BUILD_DESCRIPTION[0], false);
+  ro_prop_override(nullptr, "product", model, false);
+  ro_prop_override(nullptr, "com.google.clientidbase", CLIENT_ID[0], false);
+  property_override("ro.oem_unlock_supported", "0");
+  property_override("ro.boot.verifiedbootstate", "green");
+  property_override("ro.debuggable", "0");
+  property_override("ro.build.type", "user");
+
+  check_device();
+  SetProperty("dalvik.vm.heapstartsize", heapstartsize);
+  SetProperty("dalvik.vm.heapgrowthlimit", heapgrowthlimit);
+  SetProperty("dalvik.vm.heapsize", heapsize);
+  SetProperty("dalvik.vm.heaptargetutilization", heaptargetutilization);
+  SetProperty("dalvik.vm.heapminfree", heapminfree);
+  SetProperty("dalvik.vm.heapmaxfree", heapmaxfree);
 }
 
 void vendor_load_properties() {
-    const auto set_ro_product_prop = [](const std::string &source,
-                                        const std::string &prop,
-                                        const std::string &value) {
-        auto prop_name = "ro.product." + source + prop;
-        property_override(prop_name.c_str(), value.c_str(), false);
-    };
+  std::string variant;
+  variant = GetProperty("ro.product.vendor.name", "");
 
-    for (const auto &source : ro_props_default_source_order) {
-        set_ro_product_prop(source, "brand", "Redmi");
-        set_ro_product_prop(source, "device", "picasso");
-        set_ro_product_prop(source, "model", "Redmi K30 5G");
-    }
-    property_override("ro.build.description", "redfin-user 11 RQ1A.210105.003 7005429 release-keys");
-    property_override("ro.oem_unlock_supported", "0");
-    property_override_multifp("ro.build.fingerprint", "ro.system.build.fingerprint", "ro.bootimage.build.fingerprint",
-	    "ro.vendor.build.fingerprint", "ro.odm.build.fingerprint", "google/redfin/redfin:11/RQ1A.210105.003/7005429:user/release-keys");
-
-     // Workaround SafetyNet
-    workaround_snet_properties();
+  if (variant == "picasso") {
+    load_props(DEVICES[0], false);
+  } else if (variant == "picasso_48m") {
+    load_props(DEVICES[1], true);
+  }
 }
