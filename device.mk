075451ea74533c79ceafc587e34c4a4e137ee9d6
Manuel José Moral Eliche <az.az.az.mjme@gmail.com>
2021-03-05 16:30:02 +0100

    picasso: Cleanup & move overlay-lineage to overlay
    Signed-off-by: Manuel José Moral Eliche <az.az.az.mjme@gmail.com>

--- device.mk
+++ device.mk
@@ -38,8 +38,7 @@ PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 # Overlays -- Override vendor ones
 PRODUCT_PACKAGES += \
