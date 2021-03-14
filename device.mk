14aa408a8002755b4772e6d8d1a7b0ef03c0c468
Manuel José Moral Eliche <az.az.az.mjme@gmail.com>
2021-03-14 16:21:56 +0100

    picasso: No libfmq

--- device.mk
+++ device.mk
@@ -35,8 +35,7 @@ PRODUCT_PACKAGES += \
 # Audio
 PRODUCT_PACKAGES += \
     audio.a2dp.default \
-    libaacwrapper \
-    libfmq
+    libaacwrapper
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/audio/audio_policy_configuration.xml \
