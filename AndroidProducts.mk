e9e76d6b985ca1857ec8a3ff20809102da3fd6fd
pawelik001 <pawelik001@gmail.com>
2021-04-03 12:36:45 +0200

    juice: LineageOS bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -6,4 +6,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/legion_picasso.mk
+    $(LOCAL_DIR)/lineage_juice.mk
--- lineage_juice.mk
+++ lineage_juice.mk
@@ -9,17 +9,17 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from picasso device
-$(call inherit-product, device/xiaomi/picasso/device.mk)
+# Inherit from juice device
+$(call inherit-product, device/xiaomi/juice/device.mk)
 
-# Inherit some common Legion stuff.
-$(call inherit-product, vendor/legion/config/common_full_phone.mk)
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := legion_picasso
-PRODUCT_DEVICE := picasso
+PRODUCT_NAME := lineage_juice
+PRODUCT_DEVICE := juice
 PRODUCT_BRAND := Redmi
-PRODUCT_MODEL := K30 5G
+PRODUCT_MODEL := SM6115
 PRODUCT_MANUFACTURER := Xiaomi
 
 # Adds face unlock if package is available on ROM source.
@@ -27,9 +27,5 @@ TARGET_SUPPORT_FACE_UNLOCK := true
 
 # Boot animation
 TARGET_BOOT_ANIMATION_RES := 1080
-
-# LegionOS maintainter
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.maintainer.name=masemoel
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
