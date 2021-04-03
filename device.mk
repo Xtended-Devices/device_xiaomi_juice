fa841c066a6abece22cca8aeb73c74b8296b45c5
Joboy Jhem Francisco <joboyjhem24@gmail.com>
2021-04-03 13:36:17 +0200

    juice: Update init's and fstab

--- device.mk
+++ device.mk
@@ -107,7 +107,7 @@ PRODUCT_PACKAGES += \
 
 # Fstab
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom
+    $(LOCAL_PATH)/rootdir/etc/fstab.default:$(TARGET_COPY_OUT_RAMDISK)/fstab.default
 
 # HIDL
 PRODUCT_PACKAGES += \
