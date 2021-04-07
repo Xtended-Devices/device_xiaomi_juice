721cbd8496bb372903b82459998e8d4ad58902ac
pawelik001 <pawelik001@gmail.com>
2021-04-07 17:22:34 +0200

    juice: Android.mk fixup

--- Android.mk
+++ Android.mk
@@ -1,6 +1,5 @@
 #
-# Copyright (C) 2020 The LineageOS Project
-# Copyright (C) 2021 The LegionOS Project
+# Copyright (C) 2021 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -8,15 +7,9 @@
 LOCAL_PATH := $(call my-dir)
 
 ifneq ($(filter lemon pomelo citrus lime,$(TARGET_DEVICE)),)
-  subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
-  $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
 
-METADATA_SYMLINK := $(TARGET_ROOT_OUT)/metadata
-$(METADATA_SYMLINK): $(LOCAL_INSTALLED_MODULE)
-	@echo "Creating $@"
-	@mkdir -p $(TARGET_ROOT_OUT)/metadata
-	$(hide) ln -sf /data/vendor/metadata_apex $@/apex
+include $(CLEAR_VARS)
 
-ALL_DEFAULT_INSTALLED_MODULES += $(METADATA_SYMLINK)
+include $(call all-makefiles-under,$(LOCAL_PATH))
 
 endif
